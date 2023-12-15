cd9b90b720a8e25fbd802ea2f608f505222594b6
Azhar Aizad <aizadazhar2@gmail.com>
2023-12-15 16:32:34 +0800

    Commit to Azhar

--- src/Database_Project/MyLinkedList.java
+++ src/Database_Project/MyLinkedList.java
@@ -3,5 +3,6 @@ package Database_Project;
 public class MyLinkedList{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("test");
     }
 }
